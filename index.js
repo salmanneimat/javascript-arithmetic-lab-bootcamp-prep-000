9fb87907e7b56e7f5d5b87af48d47df96eeff8b0
Salman Ahmad <salman.neim@gmail.com>
2018-12-11 05:57:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var a=5
+var b=4 
+
+function add(a,b) {
+a+= b }
+
+function subtract(a,b) {
+a -= b }
+
+function multiple(a,b) {
+a*= b }
+
+function divide(a,b) {
+a/= b }
+
+var number= 10
+
+function add5() {
+number += 5 }
+
+function divideBy3() {
+number /= 3  }
+
+divideBy3()
+console.log(number)
+
+add5()
+console.log(number)
+
+
+
+
+
